d90ec1f504dacd24a4eddd3dbf71eec7df9f8f20
YongVuthivathnakk <vuthivathnakkyong@gmail.com>
2025-11-01 12:30:00 +0700

    update

---
+++ lib/domain/appointment.dart
@@ -0,0 +1,8 @@
+import 'package:hospital_management_dart/domain/patient.dart';
+import 'package:hospital_management_dart/domain/staff.dart';
+
+class Appointment {
+  String id;
+  Staff staff;
+  Patient patient;
+}
